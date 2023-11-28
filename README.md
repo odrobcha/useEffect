fa0ecdcffa75c811d30bc37bc89f467282aaeda0
odrobcha <drobchak.o@gmail.com>
2023-11-28 12:40:07 +0100

    ci: Add the geolocaton and sorting on it

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-Creat React Project with Vite
+##Creat React Project with Vite
 Documentation  https://vitejs.dev/guide/why.html
 - install node.js
 - `npm create vite@latest`
     - type the name
     - chose the framework
     - chose the lang
-- cd to the folderwhere the project is located
+- cd "to the folder where the project is located"
 - `npm install`
 - `npm run dev`
+
+
+## SideEffect
+Is the tasks that need to be executed that do NOT impact the current component render cycle
+
+useEffect(()=>{
+  sideEffectFunction
+}, [])
+
+useEffect function will execute if the dependency array is changed
+
+### Dependency array
+    - [] - sideEffectFunction executes once, after component function is executed
+    - null - sideEffectFunction executes every time components function rerenders
--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,104 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc';
 
-function App() {
-  const modal = useRef();
-  const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map((id) => {
+  return AVAILABLE_PLACES.find((place) => place.id === id);
+});
 
-  function handleStartRemovePlace(id) {
-    modal.current.open();
-    selectedPlace.current = id;
-  }
+function App () {
+    const modal = useRef();
+    const selectedPlace = useRef();
+    const [availablePlaces, setAvailablePlaces] = useState([]);
+    const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
-  function handleStopRemovePlace() {
-    modal.current.close();
-  }
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {    //object exposed by browser. User will be asked for permission and if permission is granted browser will grab the location. position will be past automatically
+              const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude); //async func
+              if (position) {
+                  setAvailablePlaces(sortedPlaces);
+              }
+          },
+          () => {
+              setAvailablePlaces(AVAILABLE_PLACES);
+          });
+    }, []);
 
-  function handleSelectPlace(id) {
-    setPickedPlaces((prevPickedPlaces) => {
-      if (prevPickedPlaces.some((place) => place.id === id)) {
-        return prevPickedPlaces;
-      }
-      const place = AVAILABLE_PLACES.find((place) => place.id === id);
-      return [place, ...prevPickedPlaces];
-    });
-  }
+    function handleStartRemovePlace (id) {
+        modal.current.open();
+        selectedPlace.current = id;
 
-  function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+    }
+
+    function handleStopRemovePlace () {
+        modal.current.close();
+    }
+
+    function handleSelectPlace (id) {
+        setPickedPlaces((prevPickedPlaces) => {
+            if (prevPickedPlaces.some((place) => place.id === id)) {
+                return prevPickedPlaces;
+            }
+            const place = AVAILABLE_PLACES.find((place) => place.id === id);
+            return [place, ...prevPickedPlaces];
+        });
+
+        const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+        if (!storedIds.includes(id)) {
+            localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+        }
+
+    }
+
+    function handleRemovePlace () {
+        setPickedPlaces((prevPickedPlaces) =>
+          prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+        );
+        const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+        localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
+        modal.current.close();
+    }
+
+    return (
+      <>
+          <Modal ref={modal}>
+              <DeleteConfirmation
+                onCancel={handleStopRemovePlace}
+                onConfirm={handleRemovePlace}
+              />
+          </Modal>
+
+          <header>
+              <img src={logoImg} alt="Stylized globe"/>
+              <h1>PlacePicker</h1>
+              <p>
+                  Create your personal collection of places you would like to visit or
+                  you have visited.
+              </p>
+          </header>
+          <main>
+              <Places
+                title="I'd like to visit ..."
+                fallbackText={'Select the places you would like to visit below.'}
+                places={pickedPlaces}
+                onSelectPlace={handleStartRemovePlace}
+              />
+              <Places
+                title="Available Places"
+                places={availablePlaces}
+                fallbackText="Sorting places by distance...."
+                onSelectPlace={handleSelectPlace}
+              />
+          </main>
+      </>
     );
-    modal.current.close();
-  }
-
-  return (
-    <>
-      <Modal ref={modal}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
-      </Modal>
-
-      <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
-        <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
-        </p>
-      </header>
-      <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
-          places={pickedPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-        <Places
-          title="Available Places"
-          places={AVAILABLE_PLACES}
-          onSelectPlace={handleSelectPlace}
-        />
-      </main>
-    </>
-  );
 }
 
 export default App;
