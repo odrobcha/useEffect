b6918da01ebe46c8c7a7c139b9d3a06629deac99
odrobcha <drobchak.o@gmail.com>
2023-11-28 12:56:24 +0100

    fix: on Esc close the modal

--- src/App.jsx
+++ src/App.jsx
@@ -69,10 +69,11 @@ function App () {
 
     return (
       <>
-          <Modal open={modalIsOpen}>
+          <Modal open={modalIsOpen} onClose = {handleStopRemovePlace}>
               <DeleteConfirmation
                 onCancel={handleStopRemovePlace}
                 onConfirm={handleRemovePlace}
+
               />
           </Modal>
 
